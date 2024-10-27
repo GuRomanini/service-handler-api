918f1ce509181db1ca87469f362a594e8a33a532
Gustavo Romanini <gustavo.romanini@qitech.com.br>
2024-10-27 00:05:09 -0300

    fix: lint

--- src/repositories/service_repository.py
+++ src/repositories/service_repository.py
@@ -3,6 +3,7 @@ from models import ServiceStatusModel, ServiceTypeModel
 from utils.context import Context
 from utils.logger import Logger
 
+
 class ServiceRepository:
     def __init__(self, context: Context) -> None:
         self.logger = Logger(context, __name__)
@@ -10,6 +11,8 @@ class ServiceRepository:
 
     def get_service_type_model_by_enumerator(self, enumerator: str) -> ServiceTypeModel:
         return self.context.db_session.query(ServiceTypeModel).filter(ServiceTypeModel.enumerator == enumerator).one()
-    
+
     def get_service_status_model_by_enumerator(self, enumerator: str) -> ServiceStatusModel:
-        return self.context.db_session.query(ServiceStatusModel).filter(ServiceStatusModel.enumerator == enumerator).one()
+        return (
+            self.context.db_session.query(ServiceStatusModel).filter(ServiceStatusModel.enumerator == enumerator).one()
+        )
