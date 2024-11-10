2b68182f3ccce7ccc0c9acbf4c8ef865f83d91fa
Gustavo Romanini <gustavo.romanini@qitech.com.br>
2024-11-09 23:57:17 -0300

    fix(UAVServiceController): set uav_service_key, uav_key and service_key

--- src/controllers/uav_service_controller.py
+++ src/controllers/uav_service_controller.py
@@ -1,7 +1,8 @@
+from uuid import uuid4
+
 from errors import ServiceNotFoundByName, UAVNotFound
 from mappers import UAVServiceMapper
 from models import ServiceModel, UAVModel, UAVServiceModel
-from repositories import ServiceRepository
 
 from utils.context import Context
 from utils.logger import Logger
@@ -15,7 +16,7 @@ class UAVServiceController:
     def create_by_data(self, uav_service_data: dict) -> dict:
         self.logger.info("Creating a new UAVService")
 
-        uav_model = (
+        uav_model: UAVModel = (
             self.context.db_session.query(UAVModel).filter(UAVModel.uav_key == uav_service_data["uav_key"]).first()
         )
 
@@ -33,6 +34,9 @@ class UAVServiceController:
             raise ServiceNotFoundByName(extra_fields={"service_name": uav_service_data["service_name"]})
 
         uav_service_model = UAVServiceModel()
+        uav_service_model.uav_service_key = str(uuid4())
+        uav_service_model.uav_key = uav_model.uav_key
+        uav_service_model.service_key = service_model.service_key
         uav_service_model.uav = uav_model
         uav_service_model.service = service_model
         uav_service_model.base_url = uav_service_data["base_url"]
